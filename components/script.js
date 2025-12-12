ae3f131047b0a5aa7e0e68b240c3c411f08a5001
Israr29 <im.israrahmed29@gmail.com>
2025-12-12 23:57:45 +0500

    working on my hero-section

--- components/script.js
+++ components/script.js
@@ -2,5 +2,9 @@ const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-link");
 
 hamburger.addEventListener("click", () => {
+
   navLinks.classList.toggle("active");
+    navLinks.style.transition = 1000, 'ease';
+  
+  
 });
