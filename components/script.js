6b3bce4f55fc0ed9af7c0546804d0b6dad9226c9
Israr29 <im.israrahmed29@gmail.com>
2025-12-14 23:53:19 +0500

    completed the deign and responsiveness of hero-section and navbar

--- components/script.js
+++ components/script.js
@@ -1,10 +1,10 @@
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-link");
 
-hamburger.addEventListener("click", () => {
 
-  navLinks.classList.toggle("active");
-    navLinks.style.transition = 1000, 'ease';
-  
-  
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("dropdown-mobile");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
 });
